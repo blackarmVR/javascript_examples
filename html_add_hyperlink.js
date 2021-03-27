ae178cd4bd8bdf25bb77fb0062ad2f614eee2e14
blackarmVR <blah@blah.com>
2021-03-27 14:26:22 +0000

    new

---
+++ html_add_hyperlink.js
@@ -0,0 +1,5 @@
+var targetDiv = document.getElementById("myDiv");
+var anchor = document.createElement('a');
+anchor.setAttribute('href',"yourlink.htm");
+anchor.innerText = "link text";
+mydiv.appendChild(anchor);
